16399d7f9b8bae87517bae50068a3d73b870e538
Harini Janardhan <94678338+Harini-J306@users.noreply.github.com>
2022-01-03 06:00:50 +0000

    j

--- p5original.c
+++ p5original.c
@@ -36,17 +36,4 @@ void cmp(int a, int b, int c, int *large)
     return 0;
   }
 
-  #include<stdio.h>
-int main()
-{
-  int a,b,c;
-  printf("enter 3 numbers\n");
-  scanf("%d %d %d",&a,&b,&c);
-  if((a>b)&&(a>c))
-  printf("A is big\n");
-  else if((b>a)&&(b>c))
-  printf("B is big\n");
-  else if ((c>a)&&(c>b))
-  printf("C is big\n");
-  return 0;
-}
+ 
